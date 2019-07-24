e9155a65949c1e39e6fc97f0630aff9fcca088ea
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-24 15:39:15 +0200

    modele-donnees - ajout des classes java

---
+++ src/main/java/dev/diginamic/gac/topcollegue/domain/Collegue.java
@@ -0,0 +1,98 @@
+package dev.diginamic.gac.topcollegue.domain;
+
+import java.util.Objects;
+
+public class Collegue {
+    private String id;
+    private String username;
+    private String password;
+    private String pictureUrl;
+    private String lastName;
+    private String firstName;
+
+    public Collegue() {
+    }
+
+    public Collegue(String id, String username, String password, String pictureUrl, String lastName, String firstName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.pictureUrl = pictureUrl;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Collegue collegue = (Collegue) o;
+        return Objects.equals(id, collegue.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Collegue{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", pictureUrl='").append(pictureUrl).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
