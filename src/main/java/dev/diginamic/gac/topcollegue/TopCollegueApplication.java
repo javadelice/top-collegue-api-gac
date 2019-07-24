3e66c6daa46c886cf9d9e9dffdfbf7353020a312
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-24 14:15:04 +0200

    Premier commit

---
+++ src/main/java/dev/diginamic/gac/topcollegue/TopCollegueApplication.java
@@ -0,0 +1,13 @@
+package dev.diginamic.gac.topcollegue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopCollegueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopCollegueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diginamic/gac/topcollegue/TopCollegueApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.diginamic.gac.topcollegue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopCollegueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
