af359cc557e6e75d819d85540e50fa4ac3693349
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-25 16:15:07 +0200

    Ajout de la connexion au second back si l'utilisateur n'est pas présent dans la base #4

--- src/main/java/dev/diginamic/gac/topcollegue/controller/AuthController.java
+++ src/main/java/dev/diginamic/gac/topcollegue/controller/AuthController.java
@@ -1,13 +1,13 @@
 package dev.diginamic.gac.topcollegue.controller;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
+import dev.diginamic.gac.topcollegue.controller.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +18,8 @@ import dev.diginamic.gac.topcollegue.domain.Collegue;
 import dev.diginamic.gac.topcollegue.persistence.CollegueRepository;
 import dev.diginamic.gac.topcollegue.service.CollegueService;
 import io.jsonwebtoken.Jwts;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(allowCredentials = "true")
 @RestController
@@ -31,6 +33,9 @@ public class AuthController {
 
 	@Value("${jwt.secret}")
 	private String SECRET;
+
+	@Value("${collegue.api.url}")
+	private String API_URL;
 
 	@Autowired
 	private CollegueService collegueService;
@@ -48,8 +53,41 @@ public class AuthController {
 
 	@PostMapping(value = "/auth")
 	public ResponseEntity<?> authenticate(@RequestBody Collegue infos) {
+		Optional<Collegue> optionnalCollegue = this.collegueRepository.findByUsername(infos.getUsername());
 
-		return this.collegueRepository.findByUsername(infos.getUsername())
+		// Recherche dans le deuxieme back si le collegue n'existe pas (collegue-api)
+		if (!optionnalCollegue.isPresent()) {
+			RestTemplate restTemplate = new RestTemplate();
+			try {
+				// Recuperation des cookies (si la connexion reussie)
+				List<String> cookies = restTemplate.postForEntity(API_URL + "/auth", infos, String.class).getHeaders().get("Set-Cookie");
+				String cookie = cookies.get(0).substring(0, cookies.get(0).indexOf(';'));
+
+				// Requete /me pour recuperer les donnees du collegue
+				HttpHeaders headers = new HttpHeaders();
+				headers.set("Cookie", cookie);
+				HttpEntity<UserDto> entity = new HttpEntity<>(headers);
+				UserDto user = restTemplate.exchange(API_URL + "/me", HttpMethod.GET, entity, UserDto.class).getBody();
+
+				// Creation d'un collegue a partir des donnees du /me
+				Collegue collegue = new Collegue(user.getMatricule(), user.getUsername(), passwordEncoder.encode(infos.getPassword()), user.getPictureUrl(), user.getLastName(), user.getFirstName());
+
+				// Maj de la photo si besoin
+				if(infos.getPictureUrl() != null && !infos.getPictureUrl().equals("")) {
+					collegue.setPictureUrl(infos.getPictureUrl());
+				}
+
+				// sauvegarde dans la base, puis mise au format optionnal pour la suite
+				collegueRepository.save(collegue);
+				optionnalCollegue = Optional.of(collegue);
+
+			} catch (HttpClientErrorException e) {
+				// en cas d'erreur http vers le second back, la connexion echouera (401 - unauthorized)
+			}
+		}
+
+
+		return optionnalCollegue
 		        .filter(utilisateur -> passwordEncoder.matches(infos.getPassword(), utilisateur.getPassword()))
 		        .map(utilisateur -> {
 
