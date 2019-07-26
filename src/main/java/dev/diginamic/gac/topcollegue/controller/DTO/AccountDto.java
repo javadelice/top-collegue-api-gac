fa3a2ac0acc70e56754b2d2b8b24c7c4a944e75d
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-26 12:34:35 +0200

    Ajout d'un jeu de données en lien avec collegue-api #10

---
+++ src/main/java/dev/diginamic/gac/topcollegue/controller/DTO/AccountDto.java
@@ -0,0 +1,30 @@
+package dev.diginamic.gac.topcollegue.controller.DTO;
+
+public class AccountDto {
+    private String email;
+    private String firstName;
+
+    public AccountDto() {
+    }
+
+    public AccountDto(String email, String firstName) {
+        this.email = email;
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
